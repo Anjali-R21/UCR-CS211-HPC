09b5f44a3f9a99dd2b558a0f8a648422ed1ded87
Fan Quan <fanquan@Fans-iMac.local>
2018-10-12 11:02:20 -0700

    working

--- 1/1.p1.main.cpp
+++ 1/1.p1.main.cpp
@@ -8,13 +8,21 @@
 int main()
 {
     clock_t t1, t2;
+    float sum1 = 0, sum2 = 0;    // Run 5 times and pick the average time
     for (int i = 0; i < 6; i++)
     {
-        // printf("Ns[i]=%d\n", Ns[i]);
-        t1 = dgemm0(Ns[i]);
-        t2 = dgemm1(Ns[i]);
+        for (int j = 0; j < 5; j++)
+        {
+            // printf("Ns[i]=%d\n", Ns[i]);
+            t1 = dgemm0(Ns[i]);
+            t2 = dgemm1(Ns[i]);
+            sum1 += t1;
+            sum2 += t2;
 
-        printf("When n=%d, dgemm0 takes %lu seconds, dgemm1 takes %lu seconds\n", Ns[i], t1/1000, t2/1000);
+            printf("When n=%d, dgemm0 takes %lf ms, dgemm1 takes %lf ms\n", Ns[i], (double)t1/CLOCKS_PER_SEC, (double)t2/CLOCKS_PER_SEC);
+        }
+        printf("On average, n=%d, dgemm0: %lu ms, dgemm1: %lu ms\n", Ns[i], sum1/5, sum2/5);
+        sum1 = sum2 = 0;
     }
     return 0;
 }
